4a38a7894e9dcd7c64e4a8b8176a79abb872d0ac
irunthelaw <2026phillips.junior@fcboe.org>
2024-10-11 13:41:02 -0400

    Update fade.py

--- fade.py
+++ fade.py
@@ -4,23 +4,21 @@ import time
 import random
 
 np = NeoPixel(board.D2, 30, auto_write=False, brightness=0.3)
-
 red = (255, 0, 0)
 blue = (0, 0, 255)
-green = (0, 255, 37)
+green = (0, 255, 0)
 purple = (255, 0, 255)
 yellow = (255, 255, 0)
 cyan = (0, 255, 255)
-orange = (255, 64, 0)
-val = [red, orange, yellow, green, cyan, blue, purple]
+val = [red, yellow, green, cyan, blue, purple]
 
 
-def fade_out(color, fading):
-    red_rat = color[0] / 50
+def fade_out(color, fading = 0.025, rob = 50):
+    red_rat = color[0] / rob
     red_org = color[0]
-    green_rat = color[1] / 50
+    green_rat = color[1] / rob
     green_org = color[1]
-    blue_rat = color[2] / 50
+    blue_rat = color[2] / rob
     blue_org = color[2]
     for i in range(1, 51):
         rad = red_org - i * red_rat
@@ -28,14 +26,15 @@ def fade_out(color, fading):
         blau = blue_org - i * blue_rat
         np.fill((rad, gran, blau))
         np.show()
+        print(np[0])
         time.sleep(fading)
 
-def fade_in(colors, fadings2):
-    red_rat = colors[0] / 50
+def fade_in(colors, fading = 0.025, rob = 50):
+    red_rat = colors[0] / rob
     red_org = colors[0]
-    green_rat = colors[1] / 50
+    green_rat = colors[1] / rob
     green_org = colors[1]
-    blue_rat = colors[2] / 50
+    blue_rat = colors[2] / rob
     blue_org = colors[2]
     for i in range(1, 51):
         rad = red_org + i * red_rat
@@ -43,11 +42,14 @@ def fade_in(colors, fadings2):
         blau = blue_org + i * blue_rat
         np.fill((rad, gran, blau))
         np.show()
-        time.sleep(fadings2)
+        print(np[0])
+        time.sleep(fading)
 
-while True:
-    speed = 0.01
-    rainbows = random.choice(val)
-    fade_in(rainbows, speed)
-    fade_out(rainbows, speed)
 
+def lights():
+    while True:
+        rainbows = random.choice(val)
+        fade_in(rainbows)
+        fade_out(rainbows)
+
+lights()
